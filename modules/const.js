1d1bf14d4e858ec27c287b2108aa6d6efaa0c848
Artyom <bunkov.art@list.ru>
2023-10-29 17:03:36 +0500

    Модуляция проекта

---
+++ modules/const.js
@@ -0,0 +1,5 @@
+const NAMES = ['Jack', 'Artyom9422', 'Keks', 'max', 'Cat15', 'Alice', 'Tom', 'Happy', 'epic5', 'Joe'];
+const COMMENTS = ['Great', 'cool', '...', 'beautiful!', 'кайф', 'лайк', 'good', ':)', 'первый', 'top'];
+const DESCRIPTIONS = ['Chocolate cake', 'Im at sea', 'Funny cat', 'Beautiful sky', 'Snowy January', 'Have a nice day!', 'in GTA 5', 'I hate this Monday', 'hahaha', 'im bored :('];
+
+export { NAMES, COMMENTS, DESCRIPTIONS };
