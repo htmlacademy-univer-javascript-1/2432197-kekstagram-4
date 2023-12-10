24305992ba80bab148968f7b79e265597d7d6eab
ArtyomHappy <bunkov.art@list.ru>
2023-12-10 16:09:48 +0500

    Правда или действие

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import { getPhotos } from './modules/create-photo.js';
 import { drawThumbnail } from './modules/draw-thumbnail.js';
 import { addEventListenerToPicture } from './modules/open-big-picture.js';
+import './modules/add-form.js';
 
 const photos = getPhotos();
 
